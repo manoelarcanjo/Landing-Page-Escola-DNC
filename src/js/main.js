db3845579902b72be16af51c2b1b07213497bc54
Manoel Arcanjo <codebyarcanjo@gmail.com>
2026-03-28 20:19:46 -0300

    Finalizada toda seção principal

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+var Bruna = window.document.getElementById("bruna")
+var Leonardo = window.document.getElementById("leonardo")
+var Samantha = window.document.getElementById("samantha")
+
+function Left() {
+    Bruna.style="display:none"
+    Samantha.style="display:flex"
+}
+
+function Right() {
+    Bruna.style="display:flex"
+    Samantha.style="display:none"
+}
